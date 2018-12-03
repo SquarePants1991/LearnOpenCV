e54e659fec1d445cd0915a2bfb6ab0673d9ac112
yang wang <yangwang@yangdeMacBook-Pro.local>
2018-12-03 08:46:27 +0800

    init

---
+++ Platforms/macOSX/LearnOpenCV/LearnOpenCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnOpenCV
+//
+//  Created by yang wang on 2018/12/3.
+//  Copyright © 2018 yang wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
