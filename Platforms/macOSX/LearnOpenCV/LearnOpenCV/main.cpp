e6712ccc9722e341f9d413d08d3966b3c16bcd2c
ocean <ocean@oceandeMacBook-Pro.local>
2018-12-03 21:13:46 +0800

    some eg

--- Platforms/macOSX/LearnOpenCV/LearnOpenCV/main.cpp
+++ Platforms/macOSX/LearnOpenCV/LearnOpenCV/main.cpp
@@ -2,14 +2,30 @@
 //  main.cpp
 //  LearnOpenCV
 //
-//  Created by yang wang on 2018/12/3.
-//  Copyright © 2018 yang wang. All rights reserved.
+//  Created by ocean on 2018/12/3.
+//  Copyright © 2018 handytool. All rights reserved.
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace cv;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Mat img;
+    img = imread("./Images/1.jpeg");
+    blur(img, img, Size(10, 10));
+
+    Mat grayScaleImg(img.rows, img.cols, CV_8UC1);
+    cvtColor(img, grayScaleImg, COLOR_BGR2GRAY);
+    
+    Canny(grayScaleImg, grayScaleImg, 0.1, 0.8);
+    
+    
+    
+    imshow("OpenCV Learn", grayScaleImg);
+    waitKey();
     return 0;
 }
