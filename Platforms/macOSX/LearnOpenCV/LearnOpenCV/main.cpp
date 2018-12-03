dba9f97b3d22e1890500ed1f1dce12ed400c1977
yang wang <yangwang@yangdeMacBook-Pro.local>
2018-12-03 23:32:01 +0800

    basis

--- Platforms/macOSX/LearnOpenCV/LearnOpenCV/main.cpp
+++ Platforms/macOSX/LearnOpenCV/LearnOpenCV/main.cpp
@@ -13,19 +13,56 @@
 
 using namespace cv;
 
-int main(int argc, const char * argv[]) {
-    Mat img;
-    img = imread("./Images/1.jpeg");
-    blur(img, img, Size(10, 10));
-
+void processFrame(Mat &img, Mat &result) {
     Mat grayScaleImg(img.rows, img.cols, CV_8UC1);
-    cvtColor(img, grayScaleImg, COLOR_BGR2GRAY);
+//    cvtColor(img, grayScaleImg, COLOR_BGR2GRAY);
     
-    Canny(grayScaleImg, grayScaleImg, 0.1, 0.8);
+//    Canny(grayScaleImg, grayScaleImg, 0.7, 0.8);
+//    GaussianBlur(img, img, Size(21, 21), 1);
     
+    Mat smallerImg(img.rows / 2, img.cols / 2, CV_8UC3);
+    pyrDown(img, smallerImg);
+    result = smallerImg;
+}
+
+void playVideo(const char *videoFilename) {
+    namedWindow(videoFilename);
+    VideoCapture videoCapture(videoFilename);
+//    VideoCapture videoCapture(0);
+    Mat image;
     
+    int frames = videoCapture.get(CAP_PROP_FRAME_COUNT);
+    double width = videoCapture.get(CAP_PROP_FRAME_WIDTH);
+    double height = videoCapture.get(CAP_PROP_FRAME_HEIGHT);
+    std::cout << "Total Frame: " << frames << std::endl;
     
-    imshow("OpenCV Learn", grayScaleImg);
-    waitKey();
+    int fps = videoCapture.get(CAP_PROP_FPS);
+    
+    VideoWriter videoWriter("./Output/output.mp4", VideoWriter::fourcc('M','J','P','G'), 30, Size(width, height));
+    while (1) {
+        videoCapture.read(image);
+        videoWriter.write(image);
+        processFrame(image, image);
+        imshow(videoFilename, image);
+        waitKey(1000 / fps);
+    }
+}
+
+int main(int argc, const char * argv[]) {
+//    Mat img;
+//    img = imread("./Images/2.jpg");
+//    blur(img, img, Size(10, 10));
+//
+//    Mat grayScaleImg(img.rows, img.cols, CV_8UC1);
+//    cvtColor(img, grayScaleImg, COLOR_BGR2GRAY);
+//
+//    Canny(grayScaleImg, grayScaleImg, 0.7, 0.8);
+//
+//
+//
+//    imshow("OpenCV Learn", grayScaleImg);
+//    waitKey();
+
+    playVideo("./Images/1.mp4");
     return 0;
 }
